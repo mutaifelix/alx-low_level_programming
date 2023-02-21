2cd255659700b80c59f4dad8096bd29caba4de0d
mutaifelix <felixkipkemoi6@gmail.com>
2023-02-21 12:05:05 -0800

    9-main.c

--- 0x02-functions_nested_loops/9-main.c
+++ 0x02-functions_nested_loops/9-main.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * main - check the code
  *
@@ -8,6 +10,3 @@ int main(void)
     times_table();
     return (0);
 }
-0x02$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 9-main.c 9-times_table.c -o 9-times_table
-/0x02$ ./9-times_table | cat -e
-~/0x02$ ./9-times_table | tr ' ' . | cat -e
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,29 +1,40 @@
 #include "main.h"
 
 /**
- * times_table - print the 9 times table
+ * times_table - prints the 9 times table, starting with 0
+ * Return: empty output
  */
+
 void times_table(void)
+
 {
-	int num, mult, prod;
-
-	for (num = 0; num <= 9; num++)
+	int x, y, z, u, d;
+	for (x=o; x<=9; x++)
 	{
-		_putchar('0');
-
-		for (mult = 1; mult <= 9; mult++)
-		{
-			_putchar(',');
-			_putchar(' ');
-
-			prod = num * mult;
-
-			if (prod <= 9)
-				_putchar(' ');
-			else
-				_putchar((prod / 10) + '0');
-			_putchar((prod % 10) + '0');
-		}
-		_putchar('\n');
+	for (y=0; y<=9; y++)
+	{
+	z=x*y;
+	if (z>9)
+	{
+	u=z % 10;
+	d= (z-u) / 10;
+	_putchar(44);
+	_putchar(32);
+	_putchar(d + '0');
+	_putchar(u + '0');
 	}
+	else
+	{
+	if (y ! = 0 )
+	{
+	_putchar(44);
+	_putchar(32);
+	_putchar(32);
+	}
+	_putchar(z + '0');
+	}
+	}
+	_putchar('\n');
+	}
+
 }
