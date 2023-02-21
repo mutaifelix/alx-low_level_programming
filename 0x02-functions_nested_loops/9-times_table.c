0e932c84bcacbc2e4e103594fdda83421c722c47
mutaifelix <felixkipkemoi6@gmail.com>
2023-02-21 07:44:56 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,29 +1,26 @@
 #include "main.h"
 
 /**
- * times_table - print the 9 times table
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59
  */
-void times_table(void)
+void jack_bauer(void)
 {
-	int num, mult, prod;
-
-	for (num = 0; num <= 9; num++)
-	{
-		_putchar('0');
-
-		for (mult = 1; mult <= 9; mult++)
-		{
-			_putchar(',');
-			_putchar(' ');
-
-			prod = num * mult;
-
-			if (prod <= 9)
-				_putchar(' ');
-			else
-				_putchar((prod / 10) + '0');
-			_putchar((prod % 10) + '0');
-		}
-		_putchar('\n');
-	}
+int i, j;
+i = 0;
+while (i < 24)
+{
+j = 0;
+while (j < 60)
+{
+_putchar((i / 10) + '0');
+_putchar((i % 10) + '0');
+_putchar(':');
+_putchar((j / 10) + '0');
+_putchar((j % 10) + '0');
+_putchar('\n');
+j++;
+}
+i++;
+}
 }
