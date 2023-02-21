0fdf149c6dfe1410327c09c15c10a61058d35ac2
mutaifelix <felixkipkemoi6@gmail.com>
2023-02-21 06:28:11 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,16 +7,16 @@
  */
 void print_alphabet_x10(void)
 {
-int a = 0;
-while (a < 10)
+int b = 0;
+while (b < 10)
 {
-char b = 'b';
-while (b <= 'z')
+char a = 'a';
+while (a <= 'z')
 {
-_putchar(b);
-b++;
+_putchar(a);
+a++;
 }
 _putchar('\n');
-a++;
+b++;
 }
 }
