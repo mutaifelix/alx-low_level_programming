179448aab6ad175e913de678a1512959f10ccfea
mutaifelix <felixkipkemoi6@gmail.com>
2023-02-21 05:54:35 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - check the code.
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		_putchar (alpha);
+		alpha++;
+
+	}
+	_putchar ('\n');
+
+}
