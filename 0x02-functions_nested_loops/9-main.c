57e0aa17db5ea0ab9e5d15fa898a3467fe5814ac
mutaifelix <felixkipkemoi6@gmail.com>
2023-02-21 11:33:55 -0800

    _putchar.c

---
+++ 0x02-functions_nested_loops/9-main.c
@@ -0,0 +1,13 @@
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    times_table();
+    return (0);
+}
+0x02$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 9-main.c 9-times_table.c -o 9-times_table
+/0x02$ ./9-times_table | cat -e
+~/0x02$ ./9-times_table | tr ' ' . | cat -e
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the character to print
+ * return : on success
+ * on error, -1 is returned , and errno is set appropriately
+ */
+
+int _putchar(char c)
+{
+	return (write(1,&c,1));
+
+
+
+
+
+}
