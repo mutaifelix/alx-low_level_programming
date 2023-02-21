a883fe1b27461911029c5c5db2b31ad24739a46c
mutaifelix <felixkipkemoi6@gmail.com>
2023-02-21 04:47:24 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - prints _putchar, fllowd by a new linees
+ * Return: Alwys 0 (Success)
+ */
+int main(void)
+{
+	char fel[] = "_putchar";
+
+	int f;
+
+	for (f = 0; f < 8; f++)
+	{
+		_putchar(abe[f]);
+	}
+	_putchar('\n');
+	return (0);
+}
